f9b4d412cdbc287518c775667112ae18289705d8
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-15 14:33:00 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -101,12 +101,10 @@ resource "aws_instance" "ubuntu-pub" {
   availability_zone           = data.aws_availability_zones.available.names[0]
   count                       = var.instanceCount
   subnet_id                   = aws_subnet.Public-Subnet.id
-  #vpc_id                     = "${aws_vpc.My_VPC.id}"
   user_data                   = var.userData
   tenancy                     = var.tenancy
   monitoring                  = var.monitoring
-  #ipv6_addresses             = var.ipv6_address
-  key_name                    = "ASGKEY
+  key_name                    = "ASGKEY"
   security_groups             = [aws_security_group.My_VPC_Security_Group.id]
 
   tags = {
