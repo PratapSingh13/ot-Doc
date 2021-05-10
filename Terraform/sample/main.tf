6a6d8c7719b95c3d36e27f1f5439596b54df242a
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-10 20:03:28 +0530

    Update main.tf

--- Terraform/sample/main.tf
+++ Terraform/sample/main.tf
@@ -61,3 +61,20 @@ module "target_group_and_target_group_attachment" {
   target_ids           = var.target_ids
   port                 = 80
 }
+  
+module "alb_and_alb_listeners" {
+  source                                = "github.com/PratapSingh13/ot-DocASAP//Terraform/alb"
+  alb_name                              = var.alb_name
+  internal_alb                          = var.internal
+  alb_security_groups_ids               = var.alb_security_groups_ids
+  subnets_id                            = var.subnets_id
+  enable_deletion_protection            = var.enable_deletion_protection
+  drop_invalid_header_fields            = var.drop_invalid_header_fields
+  idle_timeout                          = var.idle_timeout
+  alb_tags                              = var.alb_tags
+  alb_log_bucket                        = var.alb_log_bucket
+  alb_enable_logging                    = var.alb_enable_logging
+  target_group_arn                      = module.target_group_and_target_group_attachment.target_group_arn
+  listener_details                      = var.listener_details
+}
+
