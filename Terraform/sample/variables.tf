cead5f90857ec10487295d825a2ea872d91b07a6
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-10 18:26:45 +0530

    Update variables.tf

--- Terraform/sample/variables.tf
+++ Terraform/sample/variables.tf
@@ -10,3 +10,76 @@ variable "vpc_id" {
   type        = string
   default     = "vpc-0391267e62cf0cff1"
 }
+
+#Variables for TargetGroup
+variable "target_group_details" {
+  description = "Some essential details of TargetGroup"
+  type        = map(any)
+  default     = {
+    target_group_name     = "da-TG"
+    target_group_port     = 80
+    target_group_protocol = "HTTP"
+    target_type           = "instance"
+  }
+}
+
+variable "vpc_id" {
+  description = "VPC ID for your TargetGroup"
+  type        = string
+  default     = "vpc-0391267e62cf0cff1"
+}
+
+variable "healthy_threshold" {
+  description = "Number of consecutive health checks successes required before considering an unhealthy target healthy"
+  type        = number
+  default     = 10
+}
+
+variable "unhealthy_threshold" {
+  description = "Number of consecutive health check failures required before considering the target unhealthy"
+  type        = number
+  default     = 10
+}
+
+variable "timeout" {
+  description = "Amount of time, in seconds, during which no response means a failed health check"
+  type        = number
+  default     = 3
+}
+
+variable "interval" {
+  description = "Approximate amount of time, in seconds, between health checks of an individual target"
+  type        = number
+  default     = 30
+}
+
+variable "health_check_path" {
+  description = "Path to health-check"
+  type        = string
+  default     = "/"
+}
+
+variable "health_check_port" {
+  description = "Port to health-check"
+  type        = number
+  default     = 80
+}
+
+variable "deregistration_delay" {
+  description = "Amount time for Load Balancing to wait before changing the state of a deregistering target from draining to unused"
+  type        = number
+  default     = 90
+}
+
+variable "slow_start" {
+  description = "Amount time for targets to warm up before the load balancer sends them a full share of requests"
+  type        = number
+  default     = 100
+}
+
+#Variable for TargetGroup attachment
+variable "target_ids" {
+  description = "This is the TargetID where TG will attach"
+  type        = list(string)
+  default     = ["i-069894aabebcd6b23", "i-08ed634a42bc9859a"]
+}
