888ddd3675fa8f906d7937e9e3d894781e54fd1e
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-11 10:55:18 +0530

    Update variables.tf

--- Terraform/sample/variables.tf
+++ Terraform/sample/variables.tf
@@ -161,111 +161,111 @@ variable "listener_details" {
 ################################################
 ## Additional tag
 ################################################
-# variable "additional_tags" {
-#   type        = map(string)
-#   description = "These are the additional tags associated with the main tag of launch template"
-#   default = {
-#     Owner       = "Siddharth Gupta"
-#     Location    = "Noida"
-#     Create_By   = "Siddharth Gupta"
-#     Reviewed_By = "Rajat Vats"
-#   }
-# }
-# variable "additional_tags_asg" {
-#   type        = list(map(string))
-#   description = "These are the additional tags associated with the main tag of auto scaling group"
-#   default = [{
-#     key                 = "Owner"
-#     value               = "Siddharth Gupta"
-#     propagate_at_launch = true
-#     }, {
-#     key                 = "Location"
-#     value               = "Noida"
-#     propagate_at_launch = true
-#     },
-#     {
-#       key                 = "Create_By"
-#       value               = "Siddharth Gupta"
-#       propagate_at_launch = true
-#     },
-#     {
-#       key                 = "Reviewed_By"
-#       value               = "Rajat Vats"
-#       propagate_at_launch = true
-#     }
-#   ]
-# }
-# ################################################
-# ## Launch Template module variables
-# ################################################
-# variable "launch_template_name" {
-#   type        = string
-#   default     = "docasap_launch_template"
-#   description = "The default launch template name"
-# }
+variable "additional_tags" {
+  type        = map(string)
+  description = "These are the additional tags associated with the main tag of launch template"
+  default = {
+    Owner       = "Siddharth Gupta"
+    Location    = "Noida"
+    Create_By   = "Siddharth Gupta"
+    Reviewed_By = "Rajat Vats"
+  }
+}
+variable "additional_tags_asg" {
+  type        = list(map(string))
+  description = "These are the additional tags associated with the main tag of auto scaling group"
+  default = [{
+    key                 = "Owner"
+    value               = "Siddharth Gupta"
+    propagate_at_launch = true
+    }, {
+    key                 = "Location"
+    value               = "Noida"
+    propagate_at_launch = true
+    },
+    {
+      key                 = "Create_By"
+      value               = "Siddharth Gupta"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Reviewed_By"
+      value               = "Rajat Vats"
+      propagate_at_launch = true
+    }
+  ]
+}
+################################################
+## Launch Template module variables
+################################################
+variable "launch_template_name" {
+  type        = string
+  default     = "docasap_launch_template"
+  description = "The default launch template name"
+}
 
-# variable "iam_instance_profile_arn" {
-#   type        = string
-#   default     = ""
-#   description = "The IAM Instance Profile to launch the instance with"
-# }
+variable "iam_instance_profile_arn" {
+  type        = string
+  default     = ""
+  description = "The IAM Instance Profile to launch the instance with"
+}
 
-# variable "ami_id" {
-#   type        = string
-#   default     = "ami-09e67e426f25ce0d7"
-#   description = "The default ami id that user wants to associate with the launch template"
-# }
+variable "ami_id" {
+  type        = string
+  default     = "ami-09e67e426f25ce0d7"
+  description = "The default ami id that user wants to associate with the launch template"
+}
 
-# variable "instance_type" {
-#   type        = string
-#   default     = "t2.micro"
-#   description = "The default instance type (like micro, medium, large)  that user wants to associate with the launch template"
-# }
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "The default instance type (like micro, medium, large)  that user wants to associate with the launch template"
+}
 
-# variable "key_name" {
-#   type        = string
-#   default     = "awsKey"
-#   description = "The default key that user wants to associate with the launch template's instance"
-# }
+variable "key_name" {
+  type        = string
+  default     = "awsKey"
+  description = "The default key that user wants to associate with the launch template's instance"
+}
 
-# variable "vpc_security_group_ids" {
-#   type        = list(string)
-#   default     = ["sg-0d500ab4aadfda95a"]
-#   description = "The default list of security group that the user wants to associate with the launch template"
-# }
+variable "vpc_security_group_ids" {
+  type        = list(string)
+  default     = ["sg-0d500ab4aadfda95a"]
+  description = "The default list of security group that the user wants to associate with the launch template"
+}
 
-# variable "launch_template" {
-#   type        = string
-#   default     = ""
-#   description = "The default launch template id that user wants to associate with the launch template"
-# }
+variable "launch_template" {
+  type        = string
+  default     = ""
+  description = "The default launch template id that user wants to associate with the launch template"
+}
 ################################################
 ##Auto Scaling Group module variables
 ################################################
 
-# variable "asg_name" {
-#   type        = string
-#   default     = "docasap_asg"
-#   description = "The default auto-scaling group name"
-# }
+variable "asg_name" {
+  type        = string
+  default     = "docasap_asg"
+  description = "The default auto-scaling group name"
+}
 
-# variable "max_size" {
-#   type        = string
-#   default     = "2"
-#   description = "The maximum size of the Auto Scaling Group"
-# }
+variable "max_size" {
+  type        = string
+  default     = "2"
+  description = "The maximum size of the Auto Scaling Group"
+}
 
-# variable "min_size" {
-#   type        = string
-#   default     = "2"
-#   description = "The minimum size of the Auto Scaling Group"
-# }
+variable "min_size" {
+  type        = string
+  default     = "2"
+  description = "The minimum size of the Auto Scaling Group"
+}
 
-# variable "desired_capacity" {
-#   type        = string
-#   default     = "2"
-#   description = "The number of Amazon EC2 instances that should be running in the group"
-# }
+variable "desired_capacity" {
+  type        = string
+  default     = "2"
+  description = "The number of Amazon EC2 instances that should be running in the group"
+}
 
 # # variable "pvt_subnet_ids" {
 # #   type        = list(string)
@@ -280,8 +280,8 @@ variable "listener_details" {
 # #   description = "A aws_alb_target_group ARNs, for use with Application or Network Load Balancing"
 # # }
 
-# variable "lt_description" {
-#   type        = string
-#   default     = "sample docasap launch template"
-#   description = "Description of the launch template."
-# }
+variable "lt_description" {
+  type        = string
+  default     = "sample docasap launch template"
+  description = "Description of the launch template."
+}
