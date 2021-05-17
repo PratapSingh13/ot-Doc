c19faaf5a225d52a6e554b264e11b0c022f2397e
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-05-17 14:12:48 +0530

    Update main.tf

--- Terraform/sample/main.tf
+++ Terraform/sample/main.tf
@@ -1,7 +1,7 @@
 module "security_group" {
   source       = "../security_group"
   sg_name      = var.sg_name
-  vpc_id       = var.vpc_iD
+  vpc_id       = var.vpc_id
   sg_name_tag  = "test-sg" 
   sg_ingress   = [
     {
